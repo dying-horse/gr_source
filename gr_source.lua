25d6ff514d72d27d6694038614757da91035c2f8
Sterbendes Pferd <jfischer@cs.tu-berlin.de>
2012-03-03 09:04:49 +0100

    gr_source_blk.lua hinzugefügt

--- gr_source.lua
+++ gr_source.lua
@@ -1,4 +1,5 @@
 require "gr_source_base"
+require "gr_source_blk"
 require "gr_source_bytes"
 require "gr_source_file"
 require "gr_source_str"
@@ -6,5 +7,6 @@ require "gr_source_str"
 local mt = { __index = source }
 
 setmetatable(source.bytes, mt)
+setmetatable(source.blk, mt)
 setmetatable(source.file,  mt)
 setmetatable(source.str,   mt)
